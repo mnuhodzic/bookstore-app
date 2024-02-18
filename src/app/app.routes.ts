0c9a7d3a314a3cc14435c516977d3b6e8a9923f5
Muris Nuhodzic <muris.nuhodzic@gmail.com>
2024-02-18 11:34:19 +0100

    Inital commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  {
+    path: 'category/:name',
+    loadComponent: () =>
+      import('./category/category.component').then((m) => m.CategoryComponent),
+  },
+  {
+    path: 'product/:id',
+    loadComponent: () =>
+      import('./product/product.component').then((m) => m.ProductComponent),
+  },
+  { path: '**', component: HomepageComponent },
+];
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  api_base_url: "http://localhost:1337/graphql"
+};
